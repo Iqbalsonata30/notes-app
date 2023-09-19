d788ef49344eae16f5c7c12a4ef3ef850f3b7e94
Iqbal Sonata <iqbalsonata2@gmail.com>
2023-09-20 00:02:31 +0700

    Created Project Notes App

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log('Hallo kita akan membuat REST API');
+const hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+    const server = hapi.server({
+        port: 5000,
+        host: 'localhost',
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    });
+
+
+    server.route(routes);
+    await server.start();
+    console.log(`server running on http://${server.info.uri}`);
+};
+init();
+
+
